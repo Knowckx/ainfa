24b8db1e84e8cfc939ba6a44d72de432f3c85eb5
knowckx <knowckx@foxmail.com>
2021-07-28 18:37:07 +0800

    add print.go

---
+++ print.go
@@ -0,0 +1,47 @@
+package infa
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strconv"
+	"time"
+
+	"github.com/pelletier/go-toml/v2"
+
+	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v2"
+)
+
+func Printf(format string, a ...interface{}) (n int, err error) {
+	format = fmt.Sprintf("%s\n", format)
+	return fmt.Printf(format, a...)
+}
+
+func PrintJson(in interface{}) {
+	res, err := json.Marshal(in)
+	if err != nil {
+		log.Error().Stack().Err(err).Send()
+		return
+	}
+	Printf(string(res))
+}
+
+func PrintYaml(in interface{}) {
+	res, err := yaml.Marshal(in)
+	if err != nil {
+		log.Error().Stack().Err(err).Send()
+		return
+	}
+	Printf(string(res))
+}
+
+func PrintToml(in interface{}) {
+	res, err := toml.Marshal(in)
+	if err != nil {
+		log.Error().Stack().Err(err).Send()
+		return
+	}
+	Printf(string(res))
+}
+
---
+++ print_test.go
@@ -0,0 +1,32 @@
+package infa
+
+import (
+	"testing"
+)
+
+func TestPrint(t *testing.T) {
+	au1 := AuthParams{}
+	au1.Host = "host1"
+	au1.User = "user2"
+	au2 := AuthParams{}
+	au2.Host = "host1"
+	au2.User = "user2"
+
+	in := []*AuthParams{
+		&au1, &au2,
+	}
+	// Printf("%v", in)
+	// Print(in)
+	PrintToml(in)
+}
+
+func PanicIfErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func Test_Printf(t *testing.T) {
+	Printf("123 %s", "qq")
+	Printf("123 %s", "qq")
+	Printf("123 %s", "qq")
+}
