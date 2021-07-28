b2799216256a97d659c023a38fc4a8fa02b1525b
knowckx <knowckx@foxmail.com>
2021-07-28 16:09:52 +0800

    add toml

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.tools.sap/aeolia/in-fa
 go 1.16
 
 require (
+	github.com/pelletier/go-toml/v2 v2.0.0-beta.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rs/zerolog v1.23.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- path_test.go
+++ path_test.go
@@ -13,9 +13,8 @@ func Test_GetProjRootPath(t *testing.T) {
 }
 
 type AuthParams struct {
-	Host   string
-	User   string
-	Passwd string
+	Host string
+	User string
 }
 
 func GenTestFilesPath(fileName string) string {
@@ -28,6 +27,17 @@ func Test_LoadYamlFile(t *testing.T) {
 	out := &AuthParams{}
 	fPath := GenTestFilesPath("userinfo.yaml")
 	err := LoadYamlFile(fPath, out)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out)
+	return
+}
+
+func Test_LoadTomlFile(t *testing.T) {
+	out := map[string]AuthParams{}
+	fPath := GenTestFilesPath("azure.toml")
+	err := LoadTomlFile(fPath, &out)
 	if err != nil {
 		panic(err)
 	}
