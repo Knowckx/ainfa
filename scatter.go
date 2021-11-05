74b458eb27f1c2396334e48c65072d456987c126
cheng.zheng01 <cheng.zheng01@sap.com>
2021-11-05 15:12:03 +0800

    add get env

--- scatter.go
+++ scatter.go
@@ -1,7 +1,21 @@
 package infa
 
+import (
+	"os"
+
+	"github.com/rs/zerolog/log"
+)
+
 func PanicIfErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func GetEnv(key string) string {
+	value := os.Getenv("key")
+	if value == "" {
+		log.Warn().Str("key", key).Msg("Get environment variable failed. return nil.")
+	}
+	return value
 }
